abd61cb20fe3fb4751a3cc995edc21c8d9eb28b5
Jim Clark <jimhome1608@gmail.com>
2021-10-03 20:20:48 +1100

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { UnSplashService } from '../services/unsplash.service';
 import { WeatherService } from '../services/open-weather.service';
@@ -6,16 +7,26 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { Weather } from './weather.ts/weather.ts.component';
 import { HeaderComponent } from './header/header.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { RouterModule } from '@angular/router';
+
+const routes =
+[
+  {path:'', component:Weather},
+  {path:'forecast', component:ForecastComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     Weather,
-    HeaderComponent
+    HeaderComponent,
+    ForecastComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [UnSplashService, WeatherService],
   bootstrap: [AppComponent]
