15fe876b1a8dc39b5fc4ba8833c4e5f9d56ab383
Jim Clark <jimhome1608@gmail.com>
2021-10-05 09:28:46 +1100

    u

--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -25,7 +25,7 @@ export class ForecastComponent implements OnInit {
     let dd = String(date.getDate()).padStart(2, '0');
     let mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
     let yyyy = date.getFullYear();
-    let formattedTime = dd + '/' + mm + '/' + + yyyy;
+    let formattedTime = dd + '/' + mm + '/' +  yyyy;
 
     console.log(formattedTime);
     return formattedTime;
